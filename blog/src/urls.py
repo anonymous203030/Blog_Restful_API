14d3f8454495e99f3b90f1963bb464648f5880c5
anonymous203030 <aram.simonyan.03@gmail.com>
2020-09-15 15:04:04 +0400

    Added User Profile Added Swagger

--- blog/src/urls.py
+++ blog/src/urls.py
@@ -16,9 +16,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="Blog API Description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="https://www.linkedin.com/in/aram-simonyan-58a7281b5"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
     path('users/', include('users.urls')),
-    
+   #
+   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
